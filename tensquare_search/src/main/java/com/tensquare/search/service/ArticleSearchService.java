6c28142c177528e64a05f625eafd9e80b2faac71
Steve <yhmpc@qq.com>
2018-11-27 21:58:57 +0800

    Search

---
+++ tensquare_search/src/main/java/com/tensquare/search/service/ArticleSearchService.java
@@ -0,0 +1,26 @@
+package com.tensquare.search.service;
+
+import com.tensquare.search.dao.ArticleSearchDao;
+import com.tensquare.search.pojo.Article;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class ArticleSearchService {
+    @Autowired
+    private ArticleSearchDao articleSearchDao;
+
+    public void save(Article article) {
+        articleSearchDao.save(article);
+    }
+
+    public Page<Article> findByTitleLike(String keywords, int page, int size) {
+        Pageable pageable = PageRequest.of(page - 1, size);
+        return articleSearchDao.findByTitleOrContentLike(keywords, keywords, pageable);
+    }
+}
