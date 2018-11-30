e3ae8b0e403efe861f7ee4b11ce99e7e1c3bbe7f
yhm <yhmpc@qq.com>
2018-11-30 21:05:19 +0800

    tensquare_sms

--- tensquare_sms/src/main/java/com/tensquare/sms/listener/SmsListener.java
+++ tensquare_sms/src/main/java/com/tensquare/sms/listener/SmsListener.java
@@ -1,6 +1,7 @@
 package com.tensquare.sms.listener;
 
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
@@ -8,6 +9,12 @@ import java.util.Map;
 @Component
 @RabbitListener(queues = "sms")
 public class SmsListener {
+
+    @Value("${aliyun.sms.templateCode}")
+    private String templateCode;
+
+    @Value("${aliyun.sms.signName}")
+    private String signName;
 
     public void sendSms(Map<String, String> message) {
         System.out.println("手机号:" + message.get("mobile"));
